a900b1ec21d90d1117e5f564bd85a531e0e33a35
ks603 <kevin.shaughnessy18@gmail.com>
2020-07-29 10:43:28 -0500

    Created an array of songs. playSong function changes which song

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
+
+// Music
+const songs = [
+  {
+    name: 'Jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'Jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'Jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+]
 
 //  Check if Playing
 let isPlaying = false
@@ -9,12 +36,16 @@ let isPlaying = false
 // Play
 const playSong = () => {
   isPlaying = true
+  playBtn.classList.replace('fa-play', 'fa-pause')
+  playBtn.setAttribute('title', 'Pause')
   music.play()
 }
 
 // Pause
 const pauseSong = () => {
   isPlaying = false
+  playBtn.classList.replace('fa-pause', 'fa-play')
+  playBtn.setAttribute('title', 'Play')
   music.pause()
 }
 
@@ -26,3 +57,15 @@ playBtn.addEventListener('click', () => {
     pauseSong()
   }
 })
+
+// Update the DOM
+
+const loadSong = (song) => {
+  title.textContent = song.displayName
+  artist.textContent = song.artist
+  music.src = `music/${song.name}.mp3`
+  image.src = `img/${song.name}.jpg`
+}
+
+// On Load - Select first song
+loadSong(songs[2])
