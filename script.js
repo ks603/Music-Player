7fa32e00ebafe2f57731202aca08cc12bbfbaca7
ks603 <kevin.shaughnessy18@gmail.com>
2020-07-29 10:14:59 -0500

    Play/pause button now is functional

---
+++ script.js
@@ -0,0 +1,28 @@
+const music = document.querySelector('audio')
+const prevBtn = document.getElementById('prev')
+const playBtn = document.getElementById('play')
+const nextBtn = document.getElementById('next')
+
+//  Check if Playing
+let isPlaying = false
+
+// Play
+const playSong = () => {
+  isPlaying = true
+  music.play()
+}
+
+// Pause
+const pauseSong = () => {
+  isPlaying = false
+  music.pause()
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => {
+  if (!isPlaying) {
+    playSong()
+  } else {
+    pauseSong()
+  }
+})
